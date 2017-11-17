9552a27d705cf322a3ec5b7235f992adbda1a3c7
leodotcloud <leodotcloud@gmail.com>
2017-11-17 13:27:41 -0700

    vendor changes

--- vendor/github.com/leodotcloud/log/log.go
+++ vendor/github.com/leodotcloud/log/log.go
@@ -28,9 +28,25 @@ func Errorf(format string, args ...interface{}) {
 	stderrLogger.Errorf(format, args...)
 }
 
+// Fatalf is wrapper for logrus.Fatalf to print to stderr
+func Fatalf(format string, args ...interface{}) {
+	stderrLogger.Fatalf(format, args...)
+}
+
 // ParseLevel takes a string level and returns the Logrus log level constant.
 func ParseLevel(lvl string) (logrus.Level, error) {
 	return logrus.ParseLevel(lvl)
+}
+
+// SetLevelString takes in the log level in string format
+// some of valid values: error, info, debug ...
+func SetLevelString(lvlStr string) error {
+	level, err := ParseLevel(lvlStr)
+	if err != nil {
+		return err
+	}
+	SetLevel(level)
+	return nil
 }
 
 // SetLevel sets the log level
